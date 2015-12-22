fe57c1c2c7f2323497da81d6f592538bba09125c
REN Xiaojun <nicholas85211@gmail.com>
2015-12-22 16:03:56 +1100

    make it push based

--- AtmBar/AtmApi.swift
+++ AtmBar/AtmApi.swift
@@ -1,52 +1,65 @@
 import Foundation
+import Starscream
 
-class AtmApi {
+class AtmApi: WebSocketDelegate {
 
-    let BASE_URL = "http://52.62.29.150:8080/ts/"
-    
-    func fetchStatus(id: String, success: (Toilet) -> Void) {
-        
-        let session = NSURLSession.sharedSession()
-        let url = NSURL(string: "\(BASE_URL)\(id)")
+    let BASE_URL = "ws://52.62.29.150:8080/ts/"
+    var socket: WebSocket
 
-        let task = session.dataTaskWithURL(url!) { data, response, error in
-            // first check for a hard error
-            if let error = error {
-                NSLog("api error: \(error)")
-            }
-            
-            if let httpResponse = response as? NSHTTPURLResponse {
-                switch httpResponse.statusCode {
-                case 200:
-                    if let toilet = self.toiletFromJSONData(data!) {
-                        success(toilet)
-                    }
-                default:
-                    NSLog("api returned response: %d %@", httpResponse.statusCode, NSHTTPURLResponse.localizedStringForStatusCode(httpResponse.statusCode))
-                }
-            }
-        }
-        task.resume()
+    init(id: String){
+        let url = NSURL(string: "\(BASE_URL)\(id)/subscribe")
+        self.socket = WebSocket(url: url!)
+        socket.delegate = self
     }
-    
-    func toiletFromJSONData(data: NSData) -> Toilet? {
+
+    func subscribeOn(success: (Toilet) -> Void) {
+        socket.onText = {
+            text in
+            let toilet = self.fromJson(text)
+            success(toilet!)
+        }
+        NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("connect:"), userInfo: nil, repeats: false)
+    }
+
+    @objc func connect(socket: WebSocket) {
+        print("connecting \(self.socket)")
+        self.socket.connect()
+    }
+
+    func fromJson(str: String) -> Toilet? {
         typealias JSONDict = [String:AnyObject]
-        let json : JSONDict
-        
+        let json: JSONDict
+
         do {
-            json = try NSJSONSerialization.JSONObjectWithData(data, options: []) as! JSONDict
+            json = try NSJSONSerialization.JSONObjectWithData(str.dataUsingEncoding(NSUTF8StringEncoding)!, options: []) as! JSONDict
         } catch {
             NSLog("JSON parsing failed: \(error)")
             return nil
         }
-        
-        
-        let toilet = Toilet(
+
+        return Toilet(
             id: json["id"] as! String,
             occupied: json["occupied"] as! Bool
         )
-        
-        return toilet
     }
 
+    func websocketDidConnect(ws: WebSocket) {
+        print("websocket is connected")
+    }
+
+    func websocketDidDisconnect(ws: WebSocket, error: NSError?) {
+        if let e = error {
+            print("websocket is disconnected: \(e.localizedDescription)")
+        } else {
+            print("websocket disconnected")
+        }
+    }
+
+    func websocketDidReceiveMessage(ws: WebSocket, text: String) {
+        print("Received text: \(text)")
+    }
+
+    func websocketDidReceiveData(ws: WebSocket, data: NSData) {
+        print("Received data: \(data.length)")
+    }
 }
--- AtmBar/StatusMenuController.swift
+++ AtmBar/StatusMenuController.swift
@@ -1,44 +1,42 @@
 import Cocoa
-
+import Starscream
 
 struct Toilet {
     var id: String
     var occupied: Bool
 }
-class StatusMenuController: NSObject{
-    
+
+class StatusMenuController: NSObject {
+
     @IBOutlet weak var statusMenu: NSMenu!
-    
+
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
     let occupied = NSImage(named: "occupied")
     let vacant = NSImage(named: "vacant")
-    let atmApi: AtmApi = AtmApi()
-    
+    let atmApi: AtmApi = AtmApi(id: "1")
+
     override func awakeFromNib() {
         let icon = NSImage(named: "statusIcon")
         statusItem.image = icon
         statusItem.menu = statusMenu
-        updateStatus()
-        
-        let refreshTimer = NSTimer(timeInterval: 1.0, target: self, selector: "updateStatus", userInfo: nil, repeats: true)
-        
-        NSRunLoop.mainRunLoop().addTimer(refreshTimer, forMode:NSRunLoopCommonModes)
+        subscribeOn()
     }
 
     @IBAction func quitClicked(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self)
     }
-    
-    func updateStatus() {
-        atmApi.fetchStatus("1") { toilet in
 
-            if(toilet.occupied) {
-                NSLog("toilet on level \(toilet.id) is occupied")
-                self.statusItem.image = self.occupied
-            } else {
-                NSLog("toilet on level \(toilet.id) is vacant")
-                self.statusItem.image = self.vacant
-            }
+    func subscribeOn() {
+        atmApi.subscribeOn(statusUpdated)
+    }
+
+    func statusUpdated(toilet: Toilet) {
+        if (toilet.occupied) {
+            NSLog("toilet on level \(toilet.id) is occupied")
+            self.statusItem.image = self.occupied
+        } else {
+            NSLog("toilet on level \(toilet.id) is vacant")
+            self.statusItem.image = self.vacant
         }
     }
 }
