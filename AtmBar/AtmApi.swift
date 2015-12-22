bf9acf9ff731c476f5d51214aef8051727c4d8f3
REN Xiaojun <nicholas85211@gmail.com>
2015-12-22 16:44:33 +1100

    Load init status

--- AtmBar/AtmApi.swift
+++ AtmBar/AtmApi.swift
@@ -4,10 +4,14 @@ import Starscream
 class AtmApi: WebSocketDelegate {
 
     let BASE_URL = "ws://52.62.29.150:8080/ts/"
+    let HTTP_BASE_URL = "http://52.62.29.150:8080/ts/"
     var socket: WebSocket
+    var id: String
 
-    init(id: String){
+    init(id: String) {
         let url = NSURL(string: "\(BASE_URL)\(id)/subscribe")
+
+        self.id = id
         self.socket = WebSocket(url: url!)
         socket.delegate = self
     }
@@ -19,6 +23,35 @@ class AtmApi: WebSocketDelegate {
             success(toilet!)
         }
         NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("connect:"), userInfo: nil, repeats: false)
+
+        self.fetch(success)
+    }
+
+
+    func fetch(success: (Toilet) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: "\(HTTP_BASE_URL)\(self.id)")
+
+
+        let task = session.dataTaskWithURL(url!) {
+            data, response, error in
+            if let error = error {
+                NSLog("api error: \(error)")
+            }
+
+            if let httpResponse = response as? NSHTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200:
+                    let text = NSString(data: data!, encoding: NSUTF8StringEncoding) as! String
+                    if let toilet = self.fromJson(text) {
+                        success(toilet)
+                    }
+                default:
+                    NSLog("api returned response: %d %@", httpResponse.statusCode, NSHTTPURLResponse.localizedStringForStatusCode(httpResponse.statusCode))
+                }
+            }
+        }
+        task.resume()
     }
 
     @objc func connect(socket: WebSocket) {
@@ -38,8 +71,8 @@ class AtmApi: WebSocketDelegate {
         }
 
         return Toilet(
-            id: json["id"] as! String,
-            occupied: json["occupied"] as! Bool
+        id: json["id"] as! String,
+                occupied: json["occupied"] as! Bool
         )
     }
 
